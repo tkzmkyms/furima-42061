b6d8ea89ff7d5d480942b50cdc79d4efa153686e
tkzmkyms <tkzmkyms1002@gmail.com>
2025-06-18 05:52:40 +0900

    ルート表示の為の修正4

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
+class ItemsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get posts_index_url
+    get items_index_url
     assert_response :success
   end
 end
