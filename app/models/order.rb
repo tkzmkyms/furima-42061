8e5eacd529d987b35b00bac2a5f9c4f155e6d879
tkzmkyms <tkzmkyms1002@gmail.com>
2025-06-29 11:31:20 +0900

    2回目_ データベース削除

--- app/models/order.rb
+++
@@ -1,5 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :product
-  has_one :address
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_many :products
-  has_many :orders
-end
