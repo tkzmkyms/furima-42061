ef46c6e1c9c1d924521e0f59ca016c21d28863ad
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-05 14:26:50 +0900

    application_controller.rb修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  helper_method :current_user, :user_signed_in?
-  include Devise::Controllers::Helpers
 
   private
 
