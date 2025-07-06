f7d92e93f995fcbc0686142082efc1599728cd09
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-06 23:00:00 +0900

    販売手数料_販売利益_計算結果_ブラウザ上に描画

--- app/javascript/custom/product_price.js
+++ app/javascript/custom/product_price.js
@@ -1,3 +1,26 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+const price = () => {
+  const priceInput = document.getElementById("product-price");
+  if (!priceInput) return;
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    if (inputValue) {
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = inputValue - tax;
+      addTaxDom.innerHTML = tax;
+      profitDom.innerHTML = profit;
+    } else {
+      addTaxDom.innerHTML = '';
+      profitDom.innerHTML = '';
+    }
+  });
+};
+
+
+window.addEventListener("turbo:load", price);
+
+window.addEventListener("turbo:render", price);
+
