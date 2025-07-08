84b7143e92e3301176fe0ecd9dcf1efcc0e47eb3
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-08 17:28:19 +0900

    詳細ページのビュー編集_条件分岐によるボタンの表示の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :products, only: [:index, :new, :create, :show]
+  resources :products, only: [:index, :new, :create, :show, :edit]
 
   root "products#index"
 end
