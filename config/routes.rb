eadb50c3a6a628b598ccfdf3d7442cc65137d16d
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-10 13:29:03 +0900

    Turbo機能をオフ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :products, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :products, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:new, :create]
+  end
 
   root "products#index"
 end
-
