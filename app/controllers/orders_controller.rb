af995b16490f274c4036dea3b56d7699b9591c97
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-12 08:16:43 +0900

    コントローラーへトークンが正常に送られていることを確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,9 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+
+    binding.pry # フォームから送られたパラメータを確認
+
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
