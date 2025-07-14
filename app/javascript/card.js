f10b3990797c133ef97169a024533c04b523cf90
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-14 16:07:54 +0900

    デバックツール削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -11,22 +11,18 @@ const pay = () => {
 
   const numberElement = elements.create('cardNumber');
   numberElement.mount('#number-form');
-  console.log("✅ カード番号フォームをマウント完了");
 
   const expiryElement = elements.create('cardExpiry');
   expiryElement.mount('#expiry-form');
-  console.log("✅ 有効期限フォームをマウント完了");
 
   const cvcElement = elements.create('cardCvc');
   cvcElement.mount('#cvc-form');
-  console.log("✅ CVCフォームをマウント完了");
 
   // 購入ボタンを取得し、クリック時の処理を設定
   const submitButton = document.getElementById("button");
 
   submitButton.addEventListener("click", (e) => {
     e.preventDefault(); // フォームの通常送信をキャンセル
-    console.log("購入ボタンが押された！");
 
 
     // Payjpにカード情報のトークンを作成依頼（numberElementを渡す）
@@ -34,13 +30,10 @@ const pay = () => {
       if (response.error) {
         // トークン作成失敗時のエラーハンドリング
         alert("カード情報が正しくありません");
-        console.error("Token取得失敗", response.error);
         return;
       }
 
       // トークン作成成功時の処理
-      console.log("Token取得成功");
-      console.log(response); // トークン情報をコンソールで確認可能
 
       // トークンをhidden inputとしてフォームに追加
       const token = response.id;
