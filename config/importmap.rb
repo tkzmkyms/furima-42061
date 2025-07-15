542534b716afbf2775dc8191296c850dd6cb925f
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-15 11:14:40 +0900

    trigger_deploy

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "product_price", to: "custom/product_price.js"
 pin "card", to: "custom/card.js"
-
