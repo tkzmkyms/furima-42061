27fa4cd9d56a54f949eeb2b7c79795e5bdcbd100
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-15 16:14:51 +0900

    fix/ujs-logout-js-import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "@rails/ujs"
-import * as Turbo from "@hotwired/turbo-rails"
-import "./controllers"
-import "product_price"
-import "card"
-
-Rails.start()
