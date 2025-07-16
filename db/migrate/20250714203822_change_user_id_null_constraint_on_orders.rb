e45e94b2e52b801dbd26a59807cbd5f7d3e6a49d
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-16 20:34:06 +0900

    2回目デプロイエラー_不要なマイグレーションファイルを削除

--- db/migrate/20250714203822_change_user_id_null_constraint_on_orders.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUserIdNullConstraintOnOrders < ActiveRecord::Migration[7.1]
-  def change
-    # ordersテーブルのuser_idカラムを null不許容に変更
-    change_column_null :orders, :user_id, false
-  end
-end
