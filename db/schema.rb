4daf2ad16beb28e3a8b5e3d1cc0a1e55136b3448
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-16 20:22:47 +0900

    デプロイエラーの為_不要なマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_14_203822) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_10_223738) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_14_203822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
   end
 
