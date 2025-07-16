b60f6c173f306e29de0ef156e681b98d4cfeacad
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-16 18:58:09 +0900

    ジャバスクリプト修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
-import Rails from "@rails/ujs"
-import * as Turbo from "@hotwired/turbo-rails"
-import "./controllers"
-import "product_price"
-import "card"
+import "@hotwired/turbo-rails"
+import "controllers"
+// import "./controllers"
+import "custom/product_price"
+import "custom/card"
 
-Rails.start()
+// Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.0/lib/assets/compiled/rails-ujs.js"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "product_price", to: "custom/product_price.js"
-pin "card", to: "custom/card.js"
+
+pin_all_from 'app/javascript/custom', under: 'custom'
