345cc891a8c2a9daa7807b7ceffdc1f0c17ed0b3
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-17 17:05:48 +0900

    編集ページjavascript_都道府県名必須_修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,7 +11,7 @@ class OrderAddress
     validates :product_id
     validates :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
